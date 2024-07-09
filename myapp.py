a801d905a8b215fd1cf4c8d766f420cc18555d60
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-09 18:42:41 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -26,13 +26,13 @@ role = st.session_state.role
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 # settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
 request_1 = st.Page(
-    "request/request_1.py",
-    title="Request 1",
+    "MedQuAd/medquad.py",
+    title="Search for a Health Condition",
     icon=":material/help:",
     default=(role == "Requester"),
 )
 request_2 = st.Page(
-    "request/request_2.py", title="Request 2", icon=":material/bug_report:"
+    "MedQuAd/medquad.py", title="Request 2", icon=":material/bug_report:"
 )
 
 
