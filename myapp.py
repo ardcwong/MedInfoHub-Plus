902644b687030f5d4f99f8a5698cddff32550943
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-09 19:09:28 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -71,9 +71,9 @@ if st.session_state.role in ["Requester", "Admin"]:
 # if st.session_state.role == "Admin":
 #     page_dict["Admin"] = admin_pages
 
-# if len(page_dict) > 0:
-#     pg = st.navigation({"Account": account_pages} | page_dict)
-# else:
-#     pg = st.navigation([st.Page(login)])
+if len(page_dict) > 0:
+    pg = st.navigation({"Account": account_pages} | page_dict)
+else:
+    pg = st.navigation([st.Page(login)])
 
 pg.run()
