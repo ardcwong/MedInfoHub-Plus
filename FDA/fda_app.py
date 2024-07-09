e4be224a46a1f2987419acfd2f36e9358b0332d1
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-09 20:24:17 +0800

    Update fda_app.py

--- FDA/fda_app.py
+++ FDA/fda_app.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from skllm.config import SKLLMConfig
+from skllm.models.gpt.text2text.summarization import GPTSummarizer
+from sentence_transformers import SentenceTransformer, util
+import openai
+from openai import OpenAI
+from wordcloud import WordCloud
+import subprocess
+import time
+import numpy as np
+import ast #built in
+import chromadb
+from chromadb.utils import embedding_functions
+
+
+api_key = st.secrets['api_key']
+client = OpenAI(api_key=api_key)
+SKLLMConfig.set_openai_key(api_key)
 
