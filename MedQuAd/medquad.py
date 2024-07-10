eddbeeb34f6df505845992dd34c0e556dec99bac
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-10 14:25:06 +0800

    Update medquad.py

--- MedQuAd/medquad.py
+++ MedQuAd/medquad.py
@@ -372,10 +372,10 @@ def telemedicine():
 st.subheader("Welcome to HealthPlus!")
 inst1, inst2 = st.columns([1,10])
 inst1.caption("Instructions:")
-inst2.caption("1 | Enter a Keyword to Search"
-# inst2.caption("2 | Choose Keyword Search Method")
-# inst2.caption("3 | Choose Focus Area")
-# inst2.caption("4 | Retrieve Information about Focus Area")
+inst2.caption("1 | Enter a Keyword to Search")
+inst2.caption("2 | Choose Keyword Search Method")
+inst2.caption("3 | Choose Focus Area")
+inst2.caption("4 | Retrieve Information about Focus Area")
 
 a, b, c = st.columns([1,1,1])
 
