2fb551d4a13de449a436830ccec55c40885c625a
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-10 15:44:01 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -47,13 +47,25 @@ def contactus():
     """
     # Display formatted text with st.markdown
     st.markdown(contactinfo, unsafe_allow_html=True)
-
+def medinfohubplus():
+    st.title('MedInfoHub+')
+    # st.subheader("WHAT WE OFFER")
+    # st.image('data/use.png')
+    # st.subheader("CONTACT US")
+    # st.write('For any concerns or suggestions, you may reach out to us through the following:')
+    # contactinfo = """
+    # Facebook: facebook.com/medinfohub
+    # Twitter: twitter.com/medinfohub
+    # Instagram: instagram.com/medinfohub
+    # """
+    # # Display formatted text with st.markdown
+    # st.markdown(contactinfo, unsafe_allow_html=True)
     
 role = st.session_state.role
 
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 about_us = st.Page(contactus, title="Contact Us", icon="✉️")
-
+medinfohubplus_info = st.Page(medinfohubplus, title="About Our Data App", icon="📱")
 
 
 # settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
@@ -86,10 +98,10 @@ request_2 = st.Page(
 #     default=(role == "Admin"),
 # )
 # admin_2 = st.Page("admin/admin_2.py", title="Admin 2", icon=":material/security:")
-
+about_us_pages = [medinfohubplus_info,about_us]
 account_pages = [logout_page]
 request_pages = [request_1, request_2]
-about_us_pages = [about_us]
+
 # respond_pages = [respond_1, respond_2]
 # admin_pages = [admin_1, admin_2]
 
@@ -113,7 +125,7 @@ page_dict = {}
 if st.session_state.role in ["Patient", "Health Care Provider", "Neither"]:
     page_dict["Application"] = request_pages
 if st.session_state.role in ["Patient", "Health Care Provider", "Neither"]:
-    page_dict["About Us"] = about_us_pages
+    page_dict["MedInfoHub+"] = about_us_pages
 # if st.session_state.role in ["Responder", "Admin"]:
 #     page_dict["Respond"] = respond_pages
 # if st.session_state.role == "Admin":
