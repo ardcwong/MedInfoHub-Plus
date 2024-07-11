dee7cf65fc6c324196a5dc103625f122f760dec0
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-11 22:14:18 +0800

    Update fda_app.py

--- FDA/fda_app.py
+++ FDA/fda_app.py
@@ -226,8 +226,8 @@ if search:
     
     # st.write(top_results)
     df = pd.DataFrame(top_results, columns=["Drug_Name", "Details", "ID"])
-    st.write(df.iloc[0]["Details"].tolist())
-    keywords = extract_keywords(df.iloc[0]["Details"].tolist())
+    st.write(df.iloc[0]["Details"])
+    keywords = extract_keywords(df.iloc[0]["Details"])
     st.write(keywords)
     drug_names = df["Drug_Name"].tolist()
     choose = b.selectbox(
