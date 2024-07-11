64db0a653b7f9cd80b65c629c8798562cfc2cb6e
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-11 20:33:49 +0800

    Update fda_app.py

--- FDA/fda_app.py
+++ FDA/fda_app.py
@@ -166,18 +166,18 @@ with tab2:
     col2.markdown(content_inst, unsafe_allow_html=True)
 
 with tab3:
-    @st.experimental_dialog("Important Reminder",width="large")
-    def vote():
-        st.write("While our app provides information about illnesses and medications, it is not a substitute for professional medical advice. Self-medicating can be dangerous and may lead to serious health issues. Always consult a healthcare professional before starting or changing any medication. <br> If you are experiencing symptoms, please seek medical advice from a qualified healthcare provider. For your convenience, we have partnered with trusted clinics. Find a Partner Clinic Here.")
-        reason = st.text_input("Because...")
-        if st.button("Submit"):
-            st.session_state.vote = {"reason": reason}
-            st.rerun()
+    # @st.experimental_dialog("Important Reminder",width="large")
+    # def vote():
+    #     st.write("While our app provides information about illnesses and medications, it is not a substitute for professional medical advice. Self-medicating can be dangerous and may lead to serious health issues. Always consult a healthcare professional before starting or changing any medication. <br> If you are experiencing symptoms, please seek medical advice from a qualified healthcare provider. For your convenience, we have partnered with trusted clinics. Find a Partner Clinic Here.")
+    #     reason = st.text_input("Because...")
+    #     if st.button("Submit"):
+    #         st.session_state.vote = {"reason": reason}
+    #         st.rerun()
     
-    if tab3:
-        vote()    
-    else:
-        f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+    # if tab3:
+    #     vote()    
+    # else:
+    #     f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
     
     a, b, c = st.columns([1,1,1])
        
