6cfa1a3c4b5ef28ec4140e404ec0c0466c8200c8
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-11 21:36:23 +0800

    Update fda_app.py

--- FDA/fda_app.py
+++ FDA/fda_app.py
@@ -191,8 +191,8 @@ with tab3:
         df = pd.DataFrame(top_results, columns=["Drug_Name", "Details", "ID"])
         drug_names = df["Drug_Name"].tolist()
         choose = b.selectbox(
-                "Results",
-                (drug_names), help = f'Drug Names related to{query_text}')
+                f'Results Related to {query_text}',
+                (drug_names), help = f'Drug Names related to {query_text}')
 
 
         
