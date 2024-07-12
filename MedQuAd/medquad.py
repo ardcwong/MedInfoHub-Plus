a6e610524ccfd5c3ee77533e9ec497b1e8de4be2
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-12 17:37:21 +0800

    Update medquad.py

--- MedQuAd/medquad.py
+++ MedQuAd/medquad.py
@@ -358,29 +358,29 @@ with tab3:
                     "Choose (1) from matched Focus Area/s",
                     filtered_df["focus_area"].sort_values(ascending = True).str.lower().unique().tolist(), index=None)
             st.session_state.fac = focus_area_choose
-            if a.button("View Information", type = "primary", use_container_width = True):
+            # if a.button("View Information", type = "primary", use_container_width = True):
             
-                if focus_area_choose:
-                    focus_area, summary, filtered_df = process_keyword(keyword, df, st.session_state.fac)
-                    select_questions(filtered_df)
-                    # doctor_recommendation = specialty_doctor_recommendation(summary)
-                    # column2.markdown(doctor_recommendation)
-                    telemedicine()
-                
-                else:
-                    if st.session_state.fac == None:
-                        st.error("Please choose a focus area first.")
-        elif st.session_state.choosemethod == 'Best Match':
-            if a.button("View Information", type = "primary", use_container_width = True):
-                # # Filter questions containing the keyword
-                # filtered_df = df[df['question'].str.contains(keyword, case=False, na=False)]
-                best_match_focus_area = search_keyword(keyword, df['focus_area'])
-                focus_area, summary, filtered_df = process_keyword(keyword, df, best_match_focus_area)
+            if focus_area_choose:
+                focus_area, summary, filtered_df = process_keyword(keyword, df, st.session_state.fac)
                 select_questions(filtered_df)
+                # doctor_recommendation = specialty_doctor_recommendation(summary)
+                # column2.markdown(doctor_recommendation)
                 telemedicine()
+            
             else:
                 if st.session_state.fac == None:
                     st.error("Please choose a focus area first.")
+        elif st.session_state.choosemethod == 'Best Match':
+            # if a.button("View Information", type = "primary", use_container_width = True):
+            # # Filter questions containing the keyword
+            # filtered_df = df[df['question'].str.contains(keyword, case=False, na=False)]
+            best_match_focus_area = search_keyword(keyword, df['focus_area'])
+            focus_area, summary, filtered_df = process_keyword(keyword, df, best_match_focus_area)
+            select_questions(filtered_df)
+            telemedicine()
+            # else:
+            #     if st.session_state.fac == None:
+            #         st.error("Please choose a focus area first.")
     # else:
     #     st.info("Please enter a keyword to search.")
             
