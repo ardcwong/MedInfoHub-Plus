db20bab88716a3e9a16cf2b7085afa83312e1e55
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-12 18:09:10 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -229,8 +229,7 @@ data_apps = [medquad, fda_app]
 # st.title("Request manager")
 # st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
 st.logo(
-    "data/mihplus.png",
-    ,
+    "data/mihplus.png"
 )
 
 page_dict = {}
