e4bd2286bc1e7c0f3f52897634e8232f91ac8a65
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-12 14:13:44 +0800

    Update fda_app.py

--- FDA/fda_app.py
+++ FDA/fda_app.py
@@ -131,7 +131,7 @@ def generate_user_conversational_response(drug_name, drug_document, user_profile
     combined_messages = [
         {"role": "system", "content": "You are a medical assistant bot that generates both a summary and usage guidelines based on retrieved drug information."},
         {"role": "user", "content": f"This is the retrieved information about the drug {drug_name}: {drug_document}"},
-        {"role": "user", "content": user_tone + " Generate both a summary and usage guidelines without any unnecessary formatting. Summary should be in bullet-form."}
+        {"role": "user", "content": user_tone + " Generate both a summary and usage guidelines without any unnecessary formatting. Summary should not be in bullet-form."}
     ]
     
     combined_response = client.chat.completions.create(
@@ -261,7 +261,7 @@ if query_text:
                 column1.markdown(highlighted_summ, unsafe_allow_html=True)
 
 
-            wordcloud = WordCloud(width=800, height=400, background_color='white').generate(drug_document)
+            wordcloud = WordCloud(width=800, height=400, background_color='white').generate(" ".join(drug_document))
             st.session_state['wordcloud'] = wordcloud
 
             # Display the word cloud
