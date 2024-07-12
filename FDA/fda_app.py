3d66f3f747e21ca4e5bdfcaf402a53cc7a5dd697
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-12 16:17:07 +0800

    Update fda_app.py

--- FDA/fda_app.py
+++ FDA/fda_app.py
@@ -282,9 +282,9 @@ if query_text:
                 
                     <b>NowServing</b>: https://nowserving.ph/<br><b>Konsulta MD</b>: https://konsulta.md/<br><b>SeriousMD</b>: https://seriousmd.com/healthcare-super-app-philippines
                     """
-                    st.link_button("Now Serving", "https://nowserving.ph")
-                    st.link_button("Konsulta MD", "https://konsulta.md/")
-                    st.link_button("SeriousMD", "https://seriousmd.com/healthcare-super-app-philippines")
+                    # st.link_button("Now Serving", "https://nowserving.ph")
+                    # st.link_button("Konsulta MD", "https://konsulta.md/")
+                    # st.link_button("SeriousMD", "https://seriousmd.com/healthcare-super-app-philippines")
                     # Display formatted text with st.markdown
                     st.markdown(text, unsafe_allow_html=True)
                 telemedicine()
